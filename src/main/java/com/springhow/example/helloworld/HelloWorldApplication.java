93c3b019983521ea78a8582487388d348de8324c
Debabrata-DevOps <109823164+Debabrata-DevOps@users.noreply.github.com>
2022-08-17 01:22:08 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "Hello Bro...I am Debabrata";
+        return "Hello Bro...I am Debabrata Barui";
     }
 
 }
